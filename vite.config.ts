95ebb79a942dd8349068c85f0c6d6bb942977c5e
toumigenie-ops <toumi.genie@gmail.com>
2026-02-08 21:02:20 +0100

    Add build configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/site-burger/',
   plugins: [react()],
   root: '.',
   build: {
